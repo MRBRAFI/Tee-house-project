f79a4670955e00698af41568b989849ce4b9ddb6
learning about git <devmrbrafi@gmail.com>
2025-08-21 03:27:47 +0600

    Created the main part of tea house project

---
+++ tea-house-resources/tour_of_DOM/child.js
@@ -0,0 +1,7 @@
+const takeThechild = document.createElement('li');
+takeThechild.innerText = 'le baba pera nai';
+
+console.log(document.getElementById('parent_container').innerText);
+
+parent_container.appendChild(takeThechild);
+
